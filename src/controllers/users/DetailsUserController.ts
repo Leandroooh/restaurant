b135721fdedefa4b5fabdce7261edde137eefc78
Leandro <LeandroSilvaDeOliveira0605@gmail.com>
2025-09-30 14:36:01 -0300

    feat: login and register complete

--- src/controllers/users/DetailsUserController.ts
+++ src/controllers/users/DetailsUserController.ts
@@ -3,8 +3,10 @@ import { DetailsUserService } from "../../services/user/DetailsUserService";
 
 class DetailsUserController {
 	async handle(req: Request, res: Response) {
+		const { user_id } = req;
+
 		const detailsUserService = new DetailsUserService();
-		const user = await detailsUserService.execute();
+		const user = await detailsUserService.execute(user_id);
 
 		return res.json({ user });
 	}
--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { AuthUserController } from "./controllers/users/AuthUserController";
 import { CreateUserController } from "./controllers/users/CreateUserController";
 import { DetailsUserController } from "./controllers/users/DetailsUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
 /* User Routes */
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
-router.get("/me", new DetailsUserController().handle);
+router.get("/me", isAuthenticated, new DetailsUserController().handle);
 
 export { router };
