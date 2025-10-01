f166c30a1fa932120429f624d002d08077d37817
Leandro <LeandroSilvaDeOliveira0605@gmail.com>
2025-10-01 16:17:32 -0300

    feat: image upload added

---
+++ src/controllers/product/CreateProductController.ts
@@ -0,0 +1,28 @@
+import type { Request, Response } from "express";
+import { CreateProductService } from "../../services/product/CreateProductService";
+
+class CreateProductController {
+	async handle(req: Request, res: Response) {
+		const { name, price, description, category_id } = req.body;
+
+		const createProductService = new CreateProductService();
+
+		if (!req.file) {
+			throw new Error("upload file error!");
+		}
+
+		const { filename } = req.file;
+
+		const product = createProductService.execute({
+			name,
+			price,
+			description,
+			banner: "",
+			category_id,
+		});
+
+		res.json({ product });
+	}
+}
+
+export { CreateProductController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "./config/multer";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 import { AuthUserController } from "./controllers/users/AuthUserController";
 import { CreateUserController } from "./controllers/users/CreateUserController";
 import { DetailsUserController } from "./controllers/users/DetailsUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"));
 
 /* User Routes */
 router.post("/users", new CreateUserController().handle);
@@ -20,5 +24,13 @@ router.post(
 	new CreateCategoryController().handle,
 );
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+/* Product Routes */
+router.post(
+	"/product",
+	isAuthenticated,
+	upload.single("file"),
+	new CreateProductController().handle,
+);
 
 export { router };
