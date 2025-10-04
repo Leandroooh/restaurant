b036bd9dc406f14e01b084cfdb6760ad4ea0f2e4
Leandro <LeandroSilvaDeOliveira0605@gmail.com>
2025-10-03 23:56:10 -0300

    feat: small change

--- src/controllers/order/ListOrderController.ts
+++ src/controllers/order/ListOrderController.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 import { ListOrderService } from "../../services/order/ListOrderService";
 
 class ListOrderController {
-	async handle(req: Request, res: Response) {
+	async handle(_req: Request, res: Response) {
 		const listOrderService = new ListOrderService();
 		const orders = await listOrderService.execute();
 
